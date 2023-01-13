34bf9d5d919df18165126c6d9bbcd6b2ca4f5193
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-01-13 16:54:31 +0100

    Added HeaderSocials + icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import CTA from './CTA';
+import Me from '../../assets/me.png';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<header>
+			<div className='container header__container'>
+				<h5>Hello I'm</h5>
+				<h1>Piotr Romanczuk</h1>
+				<h5 className='text-light'>Front-End Developer</h5>
+				<CTA />
+				<HeaderSocials />
+
+				<div className='me'>
+					<img src={Me} alt='me' />
+				</div>
+				<a href='#contact' className='scroll__down'>
+					Scroll Down
+				</a>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
